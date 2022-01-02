8039bae9a48576880fefa1f1b6c5b4d944fe2020
JacobLinCool <jacoblincool@gmail.com>
2022-01-02 17:39:18 +0800

    feat: scaffold type help & version color

--- src/jacob.js
+++ src/jacob.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const { resolve } = require("path");
+const chalk = require("chalk");
 const { program } = require("commander");
 
-program.version(require("../package.json").version, "-V, -v", "output the current version");
+program.version(`Version: ${chalk.yellowBright(require("../package.json").version)}`, "-V, -v", "output the current version");
 
 program
     .command("new <type>")
     .description("setup a project")
+    .addHelpText("after", chalk`\nAvailable types: {blueBright ts}`)
     .action((type) => {
         if (fs.existsSync(resolve(__dirname, "tools", "new", type + ".js"))) {
             require(resolve(__dirname, "tools", "new", type + ".js"))();
